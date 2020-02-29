67c713a8220a4f60c794ba929ed03cdfb18ae164
kgalang <kgalang5@gmail.com>
2020-02-29 16:19:26 -0500

    Make it work.

--- src/search/TextSearcher.java
+++ src/search/TextSearcher.java
@@ -6,10 +6,11 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class TextSearcher {
 	protected ArrayList<String> tokens;
-	protected HashMap<String, ArrayList<Number>> tokenLocations;
+	protected HashMap<String, ArrayList<Integer>> tokenLocations;
 	private TextTokenizer tokenizer;
 	private String rePattern = "[a-zA-Z0-9']+";
 	/**
@@ -55,7 +56,7 @@ public class TextSearcher {
 				if (tokenExistsInMap) {
 					this.tokenLocations.get(formattedToken).add(i);
 				} else {
-					ArrayList<Number> locations = new ArrayList<>();
+					ArrayList<Integer> locations = new ArrayList<>();
 					locations.add(i);
 					this.tokenLocations.put(formattedToken, locations);
 				}
@@ -74,7 +75,65 @@ public class TextSearcher {
 	 */
 	public String[] search(String queryWord,int contextWords) {
 		// TODO -- fill in implementation
-		return new String[0];
+		List<String> results = new ArrayList<>();
+//		format query word to lowercase
+		String formattedWord = queryWord.toLowerCase().trim();
+
+//		if not in token locations, return empty array
+		if (!this.tokenLocations.containsKey(formattedWord)){
+			return results.toArray(new String[0]);
+		}
+
+//		else, get token locations
+		ArrayList<Integer> locations = this.tokenLocations.get(formattedWord);
+//		for each location
+//		get and add context for phrase
+//		add phrase to results array
+		locations.forEach((i) -> results.add(
+				_getPhrase(i, contextWords)
+		));
+
+		//		return results array
+		return results.toArray(new String[0]);
+	}
+
+	private String _getPhrase(int queryWordIndex, int contextWords) {
+		StringBuilder phrase = new StringBuilder(this.tokens.get(queryWordIndex));
+		String leftContext = _getContext(queryWordIndex, "left", contextWords);
+		String rightContext = _getContext(queryWordIndex, "right", contextWords);
+
+		phrase.insert(0, leftContext);
+		phrase.append(rightContext);
+
+		return phrase.toString();
+	}
+
+	private String _getContext(int queryWordIndex, String direction, int contextWords) {
+		StringBuilder context = new StringBuilder();
+		int moveOver = direction == "left" ? -1 : 1;
+		int contextWordCount = 0;
+		int tokenIndex = queryWordIndex + moveOver;
+
+		while (
+				contextWordCount < contextWords
+				&& tokenIndex >= 0
+				&& tokenIndex < this.tokens.size()
+		) {
+			String token = this.tokens.get(tokenIndex);
+			if (this.tokenizer.isWord(token)) {
+				contextWordCount++;
+			}
+
+			if (direction == "left") {
+				context.insert(0, token);
+			} else {
+				context.append(token);
+			}
+
+			tokenIndex += moveOver;
+		}
+
+		return context.toString();
 	}
 }
 
